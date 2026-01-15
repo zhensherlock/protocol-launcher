2534e29d18b009e036c7f0a15519bf4dfeeeb1a9
zhensherlock <zhensherlock@126.com>
2026-01-15 09:54:10 +0800

    docs(cursor): add jsdoc comments for openFolder function

--- packages/protocol-launcher/src/cursor/folder.ts
+++ packages/protocol-launcher/src/cursor/folder.ts
@@ -1,9 +1,32 @@
+/**
+ * Open folder definition.
+ */
 type OpenFolder = {
+  /**
+   * Folder path.
+   */
   path: string
+
+  /**
+   * Whether to open the folder in a new window.
+   *
+   * Defaults to `false`.
+   */
   openInNewWindow?: boolean
 }
 
+/**
+ * Open folder in Cursor
+ *
+ * @param payload Open folder definition.
+ * @returns Cursor open folder URL.
+ * @example
+ * openFolder({
+ *   path: '/etc/hosts',
+ * })
+ * // => 'cursor://file/etc/hosts'
+ */
 export function openFolder(payload: OpenFolder) {
-  const { path, openInNewWindow } = payload
+  const { path, openInNewWindow = false } = payload
   return `cursor://file${path}${openInNewWindow ? '?windowId=_blank' : ''}`
 }
