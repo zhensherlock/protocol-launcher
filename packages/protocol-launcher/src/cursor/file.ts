99f2b05b74963c66d3f33885df2e51ca2c7a423c
zhensherlock <zhensherlock@126.com>
2026-01-15 09:52:30 +0800

    docs(cursor): add jsdoc comments for openFile function

--- packages/protocol-launcher/src/cursor/file.ts
+++ packages/protocol-launcher/src/cursor/file.ts
@@ -1,12 +1,45 @@
 import { isUndefined } from '@protocol-launcher/shared'
 
+/**
+ * Open file definition.
+ */
 type OpenFile = {
+  /**
+   * File path.
+   */
   path: string
+
+  /**
+   * Line number.
+   */
   line?: number
+
+  /**
+   * Column number.
+   */
   column?: number
+
+  /**
+   * Whether to open the file in a new window.
+   *
+   * Defaults to `false`.
+   */
   openInNewWindow?: boolean
 }
 
+/**
+ * Open file in Cursor
+ *
+ * @param payload Open file definition.
+ * @returns Cursor open file URL.
+ * @example
+ * openFile({
+ *   path: '/etc/hosts',
+ *   line: 10,
+ *   column: 5,
+ * })
+ * // => 'cursor://file/etc/hosts:10:5'
+ */
 export function openFile(payload: OpenFile) {
   const { path, line, column, openInNewWindow = false } = payload
   return `cursor://file${path}${isUndefined(line) ? '' : `:${line}`}${isUndefined(column) ? '' : `:${column}`}${openInNewWindow ? '?windowId=_blank' : ''}`
