b105995e641d7adf1cdb07179450fdc45aefc98d
zhensherlock <zhensherlock@126.com>
2026-01-19 15:30:46 +0800

    feat(idea): add openFile function

---
+++ packages/protocol-launcher/src/idea/file.ts
@@ -0,0 +1,39 @@
+import { isUndefined } from '@protocol-launcher/shared'
+
+/**
+ * Open file definition.
+ */
+type OpenFile = {
+  /**
+   * File path.
+   */
+  path: string
+
+  /**
+   * Line number.
+   */
+  line?: number
+
+  /**
+   * Column number.
+   */
+  column?: number
+}
+
+/**
+ * Open file in IntelliJ IDEA
+ *
+ * @param payload Open file definition.
+ * @returns IntelliJ IDEA Open File URL.
+ * @example
+ * openFile({
+ *   path: '/etc/hosts',
+ *   line: 10,
+ *   column: 5,
+ * })
+ * // => 'idea://open?file=/etc/hosts&line=10&column=5'
+ */
+export function openFile(payload: OpenFile) {
+  const { path, line, column } = payload
+  return `idea://open?file=${path}${isUndefined(line) ? '' : `&line=${line}`}${isUndefined(column) ? '' : `&column=${column}`}`
+}
