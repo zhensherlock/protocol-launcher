e3eb1ba66c1ce48ccdfdc25b0bf55519b530875b
zhensherlock <zhensherlock@126.com>
2026-01-21 10:16:56 +0800

    feat(pycharm): add pycharm protocol launcher support

---
+++ .changeset/fifty-hats-enjoy.md
@@ -0,0 +1,5 @@
+---
+'protocol-launcher': minor
+---
+
+feat(pycharm): add pycharm protocol launcher support
--- packages/protocol-launcher/tests/pycharm.test.ts
+++ packages/protocol-launcher/tests/pycharm.test.ts
@@ -1,27 +1,27 @@
 import { describe, expect, test } from 'vitest'
-import { goland } from '../src'
+import { pycharm } from '../src'
 
-describe('goland', () => {
+describe('pycharm', () => {
   test('openFile should return a URL with path', async () => {
-    const url = goland.openFile({
+    const url = pycharm.openFile({
       path: '/etc/hosts',
     })
-    expect(url).toBe('goland://open?file=/etc/hosts')
+    expect(url).toBe('pycharm://open?file=/etc/hosts')
   })
 
   test('openFile should return a URL with path, line, and column', async () => {
-    const url = goland.openFile({
+    const url = pycharm.openFile({
       path: '/etc/hosts',
       line: 1,
       column: 2,
     })
-    expect(url).toBe('goland://open?file=/etc/hosts&line=1&column=2')
+    expect(url).toBe('pycharm://open?file=/etc/hosts&line=1&column=2')
   })
 
   test('openFolder should return a URL with path', async () => {
-    const url = goland.openFolder({
+    const url = pycharm.openFolder({
       path: '/etc',
     })
-    expect(url).toBe('goland://open?file=/etc')
+    expect(url).toBe('pycharm://open?file=/etc')
   })
 })
