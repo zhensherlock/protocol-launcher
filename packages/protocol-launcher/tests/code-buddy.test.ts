43e2ce06093893783b4f14eb41ea97b3bc809bdf
MichaelSun <zhensherlock@126.com>
2026-02-15 17:29:30 +0800

    test(protocol-launcher): add unit tests for code-buddy protocol launcher

---
+++ packages/protocol-launcher/tests/code-buddy.test.ts
@@ -0,0 +1,67 @@
+import { describe, expect, test } from 'vitest'
+import { codeBuddy } from '../src'
+
+describe('code-buddy', () => {
+  test('openFile should return a URL with path', async () => {
+    const url = codeBuddy.openFile({
+      path: '/etc/hosts',
+    })
+    expect(url).toBe('codebuddy://file/etc/hosts')
+  })
+
+  test('openFile should return a URL with path, line, column, and openInNewWindow', async () => {
+    const url = codeBuddy.openFile({
+      path: '/etc/hosts',
+      line: 1,
+      column: 2,
+      openInNewWindow: true,
+    })
+    expect(url).toBe('codebuddy://file/etc/hosts:1:2?windowId=_blank')
+  })
+
+  test('openFolder should return a URL with path and openInNewWindow', async () => {
+    const url = codeBuddy.openFolder({
+      path: '/etc',
+      openInNewWindow: true,
+    })
+    expect(url).toBe('codebuddy://file/etc?windowId=_blank')
+  })
+
+  test('openFolder should return a URL with path', async () => {
+    const url = codeBuddy.openFolder({
+      path: '/etc',
+    })
+    expect(url).toBe('codebuddy://file/etc')
+  })
+
+  test('openSettings should return a URL', async () => {
+    const url = codeBuddy.openSettings()
+    expect(url).toBe('codebuddy://settings')
+  })
+
+  test('openSettings should return a URL with openInNewWindow', async () => {
+    const url = codeBuddy.openSettings({
+      openInNewWindow: true,
+    })
+    expect(url).toBe('codebuddy://settings?windowId=_blank')
+  })
+
+  test('openRemote should return a URL with type, host, and path', async () => {
+    const url = codeBuddy.openRemote({
+      type: 'ssh-remote',
+      host: 'root@172.18.105.209:22',
+      path: '/code/my-project',
+    })
+    expect(url).toBe('codebuddy://vscode-remote/ssh-remote+root@172.18.105.209:22/code/my-project')
+  })
+
+  test('openRemote should return a URL with type, host, path and openInNewWindow', async () => {
+    const url = codeBuddy.openRemote({
+      type: 'ssh-remote',
+      host: 'root@172.18.105.209:22',
+      path: '/code/my-project',
+      openInNewWindow: true,
+    })
+    expect(url).toBe('codebuddy://vscode-remote/ssh-remote+root@172.18.105.209:22/code/my-project?windowId=_blank')
+  })
+})
