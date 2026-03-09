c1061d212daca17d21acf33e109b076d802b85f7
MichaelSun <zhensherlock@126.com>
2026-03-10 00:41:50 +0800

    test: add unit test for hbuilderx protocol launcher

--- packages/protocol-launcher/tests/hbuilderx.test.ts
+++ packages/protocol-launcher/tests/hbuilderx.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from 'vitest'
 import { hbuilderx } from '../src'
 
-describe('goland', () => {
+describe('hbuilderx', () => {
   test('open should return a URL', async () => {
     const url = hbuilderx.open()
     expect(url).toBe('hbuilderx://')
